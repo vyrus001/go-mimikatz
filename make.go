798111514764873ae5c0e1a363eb87ca92c713a9
Jens (ryd) Muecke <jens@nons.de>
2021-11-04 02:41:09 +0100

    Fix packer output location
    * Set parameter for packer to stub folder
    * payload got loaded in stub via embed

--- make.go
+++ make.go
@@ -1,5 +1,5 @@
 package main
 
-//go:generate go run packer/packer.go
+//go:generate go run packer/packer.go -o stub
 //go:generate go build -o stub/stub github.com/vyrus001/go-mimikatz/stub
 //go:generate go run fixer/fixer.go -s stub/stub -o go-mimikatz.exe
